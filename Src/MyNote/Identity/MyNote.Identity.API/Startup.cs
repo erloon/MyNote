90f1912bf9b955d0a84cc383df01a7aeddcaa472
Maciej Kryca <maciej.kryca@gmail.com>
2017-11-05 16:45:06 +0100

    Cheng migration data place

--- Src/MyNote/Identity/MyNote.Identity.API/Startup.cs
+++ Src/MyNote/Identity/MyNote.Identity.API/Startup.cs
@@ -1,68 +1,88 @@
-﻿using System.Linq;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using MyNote.Identity.Domain.Model;
-using MyNote.Identity.Infrastructure;
+using MyNote.Identity.API.Services;
 using System.Reflection;
 using IdentityServer4.EntityFramework.DbContexts;
 using IdentityServer4.EntityFramework.Mappers;
+using MyNote.Identity.Domain.Model;
+using MyNote.Identity.Infrastructure.MigrationData;
 
 namespace MyNote.Identity.API
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            string identityConnectionString = Configuration.GetConnectionString("IdentityConnection");
+             string connectionString = Configuration.GetConnectionString("IdentityConnection");
             var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
 
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(identityConnectionString));
+                options.UseSqlServer(connectionString));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            services.AddMvc();
+
+           
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddAspNetIdentity<ApplicationUser>()
                 .AddConfigurationStore(options =>
                 {
                     options.ConfigureDbContext = builder =>
-                        builder.UseSqlServer(identityConnectionString,
+                        builder.UseSqlServer(connectionString,
                             sql => sql.MigrationsAssembly(migrationsAssembly));
                 })
                 .AddOperationalStore(options =>
                 {
                     options.ConfigureDbContext = builder =>
-                        builder.UseSqlServer(identityConnectionString,
+                        builder.UseSqlServer(connectionString,
                             sql => sql.MigrationsAssembly(migrationsAssembly));
 
                     options.EnableTokenCleanup = true;
                     options.TokenCleanupInterval = 30;
                 });
 
-
+            services.AddSingleton<IEmailSender, EmailSender>();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+      public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
-            app.UseDatabaseErrorPage();
-            app.UseStaticFiles();
-            app.UseIdentityServer();
-
-            app.Run(async (context) =>
+            if (env.IsDevelopment())
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
             });
         }
 
@@ -103,5 +123,6 @@ namespace MyNote.Identity.API
                 }
             }
         }
+
     }
 }
