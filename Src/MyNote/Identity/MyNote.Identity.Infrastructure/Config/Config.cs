376d19cb118e9703a1cc4e2f1e0b54b7332278cc
Maciej Kryca <maciej.kryca@gmail.com>
2017-12-03 18:43:13 +0100

    Add new api and gitignore

---
+++ Src/MyNote/Identity/MyNote.Identity.Infrastructure/Config/Config.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace MyNote.Identity.Infrastructure.Config
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("Notes","Notes service"),
+                new ApiResource("Review", "Review service")
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetResources()
+        {
+            return new List<IdentityResource>()
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients(Dictionary<string, string> clientsUrl)
+        {
+            return new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId = "SpaUI",
+                    ClientName = "MyNote SPA UI",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+                    RedirectUris = {$"{clientsUrl["SpaUI"]}/"},
+                    RequireConsent = false,
+                    PostLogoutRedirectUris = {$"{clientsUrl["SpaUI"]}/"},
+                    AllowedCorsOrigins = {$"{clientsUrl["SpaUI"]}"},
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "Notes",
+                        "Review"
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ Src/MyNote/Identity/MyNote.Identity.Infrastructure/Data/ConfigurationDbContextSeed.cs
@@ -0,0 +1,50 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.Mappers;
+
+namespace MyNote.Identity.Infrastructure.Data
+{
+    public class ConfigurationDbContextSeed
+    {
+        public async Task SeedAsync(ConfigurationDbContext context, IConfiguration configuration)
+        {
+            var clientUrls = new Dictionary<string, string>();
+
+            clientUrls.Add("SpaUI", configuration.GetSection("SpaClient").Value);
+            clientUrls.Add("NotesAPI", configuration.GetSection("NotesAPIClient").Value);
+            clientUrls.Add("ReviewAPI", configuration.GetSection("ReviewAPIClient").Value);
+
+            if (!context.Clients.Any())
+            {
+                foreach (var client in Config.Config.GetClients(clientUrls))
+                {
+                    await context.Clients.AddAsync(client.ToEntity());
+                }
+                await context.SaveChangesAsync();
+            }
+
+
+            if (!context.IdentityResources.Any())
+            {
+                foreach (var resource in Config.Config.GetResources())
+                {
+                    await context.IdentityResources.AddAsync(resource.ToEntity());
+                }
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.ApiResources.Any())
+            {
+                foreach (var api in Config.Config.GetApis())
+                {
+                    await context.ApiResources.AddAsync(api.ToEntity());
+                }
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
